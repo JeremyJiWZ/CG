697c1784878d659d8fb839182ee922965f4e9c50
JeremyJiWZ <mr.ji@icloud.com>
2015-11-30 15:14:52 +0800

    实验4，通过tablelist画动画加速

---
+++ stanford_bunny.h
@@ -0,0 +1,16 @@
+//
+//  stanford_bunny.h
+//  lab4
+//
+//  Created by mr.ji on 15/11/29.
+//  Copyright (c) 2015年 mr.ji. All rights reserved.
+//
+
+#ifndef __lab4__stanford_bunny__
+#define __lab4__stanford_bunny__
+
+#include <GLUT/GLUT.h>
+
+void DrawBunny();
+
+#endif /* defined(__lab4__stanford_bunny__) */
